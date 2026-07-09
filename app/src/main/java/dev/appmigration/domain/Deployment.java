7415632e4078a46f1f56b67acdc2060a2dca5f87
Hero-Xero <omarahmedasaad@gmail.com>
2026-07-10 02:12:36 +0300

    fixed infinite recursison in the deployment domain

--- app/src/main/java/dev/appmigration/domain/Deployment.java
+++ app/src/main/java/dev/appmigration/domain/Deployment.java
@@ -1,12 +1,28 @@
 package dev.appmigration.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
-
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
+
+import org.hibernate.proxy.HibernateProxy;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(
@@ -28,6 +44,7 @@ public class Deployment {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "application_id", nullable = false)
     @ToString.Exclude
+    @JsonIgnore
     private Application application;
 
     @Column(nullable = false)
